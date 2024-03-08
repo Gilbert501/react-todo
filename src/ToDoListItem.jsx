eec2e2632895ff3818356d688ca0aa12fbb78cca
Gilbert501 <geepas1971@gmail.com>
2024-03-08 13:12:24 -0500

    updated app.jsx, to do list, to do list item and add to form files

--- src/ToDoListItem.jsx
+++ src/ToDoListItem.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
-const ToDoListItem = (props) => {
+const ToDoListItem = ({id, title}) => {
   return (
     <ul>
-      <li key={props.id}>{props.title}</li>
+      <li key={id}>{title}</li>
     </ul>
  );
 };
